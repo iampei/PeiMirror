aa695db23c07d2a552142f75d26bbaa6903fc646
Pei Yu Lin <pyl@uw.edu>
2016-05-15 11:32:44 -0700

    added dictionary

--- mirror.py
+++ mirror.py
@@ -17,17 +17,19 @@ app.config.from_object(__name__)
 
 @app.route("/")
 def hello():
-    time_string = datetime.datetime.now().strftime("%a, %b-%d-%Y, %I:%M %p")
     current_weather = get_weather.get_current_conditions()
+    time_string = datetime.datetime.now().strftime("%a, %b-%d-%Y, %I:%M %p")
+    current_weather['time'] = time_string
     hour = datetime.datetime.now().strftime("%H")
-    word_string = 'Hey, you look nice!'
+    word_string = ['Hey, you look nice!']
     if int(hour) <17:
         word_string = ['Have a good afternoon!', 'You are a super star!']
     if 17 < int(hour) < 19:
         word_string = ['Go out and have fun!', 'Have a nice evening!']
     if int(hour) >= 19:
         word_string = ['Hello, you are awesome!', 'Have a nice night!', 'You look nice!']
-    return render_template('index.html', time = time_string, weather = current_weather, words = random.choice(word_string))
+    current_weather['compliment'] = random.choice(word_string)
+    return render_template('index.html', **current_weather)
 
 
 if __name__ == '__main__':

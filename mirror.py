a55e2acb99ed0ae210f3b9b9dbd06f1db3d41ac9
Pei Yu Lin <pyl@uw.edu>
2016-05-17 20:55:33 -0700

    updated

--- mirror.py
+++ mirror.py
@@ -30,17 +30,11 @@ def get_compliment():
 @app.route("/")
 def hello():
     current_weather = get_weather.get_current_conditions()
-    time_string = datetime.datetime.now().strftime("%a, %b-%d-%Y, %I:%M %p")
+    #time_string = datetime.datetime.now().strftime("%a, %b-%d-%Y, %I:%M %p")
+    time_string = datetime.datetime.now().strftime("%I:%M %p")
+    date_string = datetime.datetime.now().strftime("%a, %b-%d-%Y")
     current_weather['time'] = time_string
-    #hour = datetime.datetime.now().strftime("%H")
-    # word_string = ['Hey, you look nice!']
-    # if int(hour) <17:
-    #     word_string = ['Have a good afternoon!', 'You are a super star!']
-    # if 17 < int(hour) < 19:
-    #     word_string = ['Go out and have fun!', 'Have a nice evening!']
-    # if int(hour) >= 19:
-    #     word_string = ['Hello, you are awesome!', 'Have a nice night!', 'You look nice!']
-    #current_weather['compliment'] = random.choice(word_string)
+    current_weather['date'] = date_string
     current_weather['compliment'] = get_compliment()
     return render_template('index.html', **current_weather)
 
@@ -51,10 +45,16 @@ def test():
 
 @app.route('/_get_update')
 def _get_update():
-    time_string = datetime.datetime.now().strftime("%a, %b-%d-%Y, %I:%M %p")
+    #current_weather = get_weather.get_current_conditions()
+    #time_string = datetime.datetime.now().strftime("%a, %b-%d-%Y, %I:%M %p")
+    time_string = datetime.datetime.now().strftime("%I:%M %p")
+    date_string = datetime.datetime.now().strftime("%a, %b-%d-%Y")
     compliment = get_compliment()
+    #weather_update = current_weather['weather']
+    #temp_update = current_weather['current_temp']
+
     #print('_get_time called')
-    return jsonify(time=time_string, compliment = compliment)
+    return jsonify(time=time_string, date = date_string, compliment = compliment) #weather = weather_update, #temp = temp_update)
 
 if __name__ == '__main__':
     app.run()
